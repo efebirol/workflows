3a03e2ada0b463df26b870168f8c70c8371f4e61
Birol <tw04n202@gmail.com>
2017-03-04 17:38:20 +0100

    sass and compass

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
     concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
@@ -11,6 +12,8 @@ var jsSources = [
   'components/scripts/tagline.js',
   'components/scripts/template.js'
 ];
+var sassSources = ['components/sass/style.scss'];
+
 
 //can be called in the command with "gulp task"
 gulp.task('log', function() {
@@ -35,5 +38,18 @@ gulp.task('js', function() {
   gulp.src(jsSources)
     .pipe(concat('script.js'))
     .pipe(browserify())
-    .pipe(gulp.dest('builds/development/js'))
+    .pipe(gulp.dest('builds/development/js'))   //put it in the "js" folder
 });
+
+//merges the style.scss with the other SASS Styles and puts them in the CSS folder
+gulp.task('compass', function() {
+  gulp.src(sassSources)
+    .pipe(compass({                 //configure the compass for the sass (where to find what)
+        sass: 'components/sass',
+        image: 'builds/development/images',
+        style: 'expanded'     //style how to indent the CSS (predefined in 'expanded', 'nested', 'compact' and 'compressed')
+    })
+    .on('error', gutil.log))
+    .pipe(gulp.dest('builds/development/css'))  //put it in the "css" folder
+});
+

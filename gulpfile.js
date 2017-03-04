3f711915934ce55beb31d1c45b3fd8a95e2ec314
Birol <tw04n202@gmail.com>
2017-03-04 16:36:50 +0100

    Concatenating JavaScript Files in a single scripts.js file

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,19 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
 
-var coffeeSources = ['components/coffee/tagline.coffee'];   //Benefit: Can now add multiple files in the array
-
+var coffeeSources = ['components/coffee/tagline.coffee'];
+var jsSources = [
+    'components/scripts/rclick.js',
+    'components/scripts/pixgrid.js',
+    'components/scripts/tagline.js',
+    'components/scripts/template.js'
+];
 
 //can be called in the command with "gulp task"
 gulp.task('log', function() {
-  gutil.log('Workflows are awesome (see gulpfile.js for more). This is a task that will be worked through.');
+    gutil.log('Workflows are awesome (see gulpfile.js for more). This is a task that will be worked through.');
 });
 
 //my own gult task (using an anonymous function)
@@ -17,8 +23,16 @@ gulp.task('birolcalls', function(){
 );
 
 gulp.task('coffee', function() {
-  gulp.src(coffeeSources)
-    .pipe(coffee({ bare: true })        //see coffee API to learn more
-      .on('error', gutil.log))          //handle the error in a log(so error won't stop this task)
-    .pipe(gulp.dest('components/scripts'))  //puts file in this folder (where our javascript files are)
+    gulp.src(coffeeSources)
+        .pipe(coffee({ bare: true })        //see coffee API to learn more
+            .on('error', gutil.log))          //handle the error in a log(so error won't stop this task)
+        .pipe(gulp.dest('components/scripts'))  //puts file in this folder (where our javascript files are)
 });
+
+//combine all the scripts into one file(uncompressed in development). Benefit: Better for Debugging (in Dev)
+gulp.task('js', function() {
+    gulp.src(jsSources)
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('builds/development/js'))
+});
+

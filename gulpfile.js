237006c6850788c5db9070da56573e2addedc8f0
Birol <tw04n202@gmail.com>
2017-03-15 20:01:25 +0100

    change to refugee site

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,12 @@ var gulp = require('gulp'),
     browserify = require('gulp-browserify'),
     compass = require('gulp-compass'),
     connect = require('gulp-connect'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-html'),
+    jsonminify = require('gulp-jsonminify'),
+    imagemin = require('gulp-imagemin'),
+    pngcrush = require('imagemin-pngcrush'),
     concat = require('gulp-concat');
 
 var env,
@@ -20,11 +26,11 @@ env = process.env.NODE_ENV || 'development';        //check if "NODE_ENV" is set
 if (env==='development') {
     outputDir = 'builds/development/';
     sassStyle = 'expanded';
-    gutil.log("-- Building in Development");
+    gutil.log("-- Building in "+outputDir);
 } else {
     outputDir = 'builds/production/';
     sassStyle = 'compressed';
-    gutil.log("-- Building in Production");
+    gutil.log("-- Building in "+outputDir);
 }
 
 
@@ -65,6 +71,7 @@ gulp.task('js', function () {
     gulp.src(jsSources)
         .pipe(concat('script.js'))
         .pipe(browserify())
+        .pipe(gulpif(env === 'production', uglify()))       //run 'uglifiy' when production (in order to minimize JS)
         .pipe(gulp.dest(outputDir+'js'))   //put it in the "js" folder
         .pipe(connect.reload())                     //reload the webpage
 });
@@ -88,28 +95,44 @@ gulp.task('watch', function () {
     gulp.watch(coffeeSources, ['coffee']);    //Info: coffee is here a gulp task [<'gulp task'>]
     gulp.watch(jsSources, ['js']);
     gulp.watch('components/sass/*.scss', ['compass']);  //watches changes in any SCSS file and runs 'compass'-Task
-    gulp.watch(htmlSources, ['html']);
+    gulp.watch('builds/development/*.html', ['html']);
     gulp.watch(jsonSources, ['json']);
     gutil.log('- watch task - watching now all the tasks');
 });
 
 gulp.task('connect', function() {
-    connect.server({
-        root: outputDir,
-        livereload: true
-    });
+  connect.server({
+    root: outputDir,
+    livereload: true
+  });
 });
 
 //Reload if changes are made in the HTML
-gulp.task('html', function () {
-    gulp.src(htmlSources)
+gulp.task('html', function() {
+    gulp.src('builds/development/*.html')
+        .pipe(gulpif(env === 'production', minifyHTML()))   //compress HTML
+        .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
         .pipe(connect.reload())
 });
 
 //Reload if changes are made in the JSON
-gulp.task('json', function () {
-    gulp.src(jsonSources)
+gulp.task('json', function() {
+    gulp.src('builds/development/js/*.json')
+        .pipe(gulpif(env === 'production', jsonminify()))   //compress JSON
+        .pipe(gulpif(env === 'production', gulp.dest('builds/production/js')))
         .pipe(connect.reload())
 });
 
-gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']);
+//Minify the images
+gulp.task('images', function() {
+    gulp.src('builds/development/images/**/*.*')    // '**/*.*' any (sub)directories and any files inside
+        .pipe(gulpif(env === 'production', imagemin({
+            progressive: true,
+            svgoPlugins: [{ removeViewBox: false }],
+            use: [pngcrush()]
+        })))
+        .pipe(gulpif(env === 'production', gulp.dest(outputDir + 'images')))
+        .pipe(connect.reload())
+});
+
+gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'images', 'connect', 'watch']);
